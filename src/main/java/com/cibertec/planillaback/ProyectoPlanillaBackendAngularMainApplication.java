a3550c170aa4e133749be698ba804bda25d29c4c
Mauricio62 <ericksandovalv08@gmail.com>
2025-12-09 00:47:34 -0500

    Estructura inicial del proyecto con dependencias de Maven

---
+++ src/main/java/com/cibertec/planillaback/ProyectoPlanillaBackendAngularMainApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.planillaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPlanillaBackendAngularMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPlanillaBackendAngularMainApplication.class, args);
+	}
+
+}
