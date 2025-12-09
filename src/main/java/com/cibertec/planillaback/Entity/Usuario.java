81420ec31d6dfc34486b595e9697fc447c77dbda
Mauricio62 <ericksandovalv08@gmail.com>
2025-12-09 16:45:54 -0500

    feat: [T2] Implementar la base del sistema de autenticación: - Entidad de usuario con la implementación de UserDetails de Spring Security - Repositorio de usuarios para operaciones de base de datos - DTO para solicitudes/respuestas de autenticación - Enumeración de roles para permisos de usuario - Utilidad de validación de correo electrónico - Definición de la interfaz AuthService

---
+++ src/main/java/com/cibertec/planillaback/Entity/Usuario.java
@@ -0,0 +1,76 @@
+package com.cibertec.planillaback.Entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.cibertec.planillaback.Utils.Enums.RolUsuarioEnum;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "usuario")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RolUsuarioEnum role;
+
+    private boolean enabled = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {   
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {   
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() {  
+        return this.enabled;
+    }
+    
+}
